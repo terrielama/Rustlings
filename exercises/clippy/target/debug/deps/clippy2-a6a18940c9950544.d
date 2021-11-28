e98f5482f5af1fc456c726d49078cfd123c5b8a5
terrielama <60408333+terrielama@users.noreply.github.com>
2021-11-29 00:44:02 +0100

    Delete exercises/clippy directory

--- exercises/clippy/target/debug/deps/clippy2-a6a18940c9950544.d
+++
@@ -1,5 +0,0 @@
-/Users/user/Desktop/S1_L2/prog_avancee_rust/rustlings/exercises/clippy/target/debug/deps/clippy2-a6a18940c9950544.rmeta: clippy2.rs
-
-/Users/user/Desktop/S1_L2/prog_avancee_rust/rustlings/exercises/clippy/target/debug/deps/clippy2-a6a18940c9950544.d: clippy2.rs
-
-clippy2.rs:
